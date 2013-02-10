dc404ff06b42f7ca2db6fed38d3281d99c914dba
Derek Worthen <derek@derekworthen.com>
2013-02-10 00:29:55 -0800

    Can now flatten arrays or leave alone

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
-var flatten = module.exports = function (obj, into, clone, seperator, prefix) {
-  into = !clone ? obj : into || {};
-  prefix = prefix || "";
-  seperator = seperator || '.';
+var flatten = module.exports = function (obj, options) {
+  var _into = obj; // = options && !options.clone ? obj : options.into || {};
+  var _prefix = (options && options.prefix) ? options.prefix : "";
+  var _seperator = (options && options.seperator) ? options.seperator : '.';
+  var _ignoreArrays = (options && options.ignoreArrays) ? true : false;
+
+  if (options && options.clone) {
+    _into = options.into || {};
+  } 
+
+  var opts = {
+    into: _into,
+    clone: true,
+    seperator: _seperator,
+    ignoreArrays: _ignoreArrays
+  };
+
+  // console.log('into: %s, pre: %s, sep: %s, ig: %s',_into, _prefix, _seperator, _ignoreArrays);
 
   for (var k in obj) {
+    opts.prefix = _prefix + k + _seperator;
     if (obj.hasOwnProperty(k)) {
       var prop = obj[k];
       if (prop && typeof prop === "object" &&
-        !(prop instanceof Date || prop instanceof RegExp || prop instanceof Array)) {
-        flatten(prop, into, true, seperator, prefix + k + seperator);
+        !(prop instanceof Date || prop instanceof RegExp)) {
+          if(_ignoreArrays) {
+            if(!(prop instanceof Array)) {
+              flatten(prop, opts);
+            } else {
+              _into[_prefix + k] = prop;
+            }
+          } else {
+            flatten(prop, opts);  
+          }
       }
       else {
-        into[prefix + k] = prop;
+        _into[_prefix + k] = prop;
       }
     }
   }
 
-  return into;
+  // console.log(_into);
+
+  return _into;
 }
